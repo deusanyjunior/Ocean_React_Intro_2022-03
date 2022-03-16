09dbc123cecfc8d9e6a052e804f690340a78badf
dj <dj@ime.usp.br>
2022-03-16 15:45:02 -0300

    Criando funcoes!

--- src/index.js
+++ src/index.js
@@ -1,7 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function MenuTopo() {
+    return (
+        <div>
+            <button>Login</button>
+            <button>Ajuda</button>
+        </div>
+    )
+}
+
+function Conteudo() {
+    return (
+        <div>Esta é minha primeira página com componentes!</div>
+    )
+}
+
+function Rodape() {
+    return (
+        <div>Copyleft by djota</div>
+    )
+}
+
+function Corpo() {
+    return (
+        <>
+            <MenuTopo />
+            <Conteudo />
+            <Rodape />
+        </>
+    )
+}
+
 ReactDOM.render(
-    <p>Olá!</p>,
-    document.getElementById('root')
+    <Corpo />
+    , 
+    document.getElementById('root')  
 );
+
