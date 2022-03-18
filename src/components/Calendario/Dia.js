4440e090307e9d258dd62acc71645ef506bbba07
dj <dj@ime.usp.br>
2022-03-18 15:22:30 -0300

    Modificando Dia de function para class

--- src/components/Calendario/Dia.js
+++ src/components/Calendario/Dia.js
@@ -1,7 +1,42 @@
-function Dia({diaDaSemana}) {
+import { Component } from "react";
+
+class Dia extends Component {
+
+    constructor () {
+        super();
+        this.aoClicar = this.aoClicar.bind(this);
+    }
+
+    state = { qtdCliques: 0}
+
+    aoClicar(event) {
+        const cliques = this.state.qtdCliques + 1;
+        this.setState(
+            { qtdCliques: cliques },
+            function () {
+                console.log("Callback - " + this.props.data + ": " + this.state.qtdCliques);
+            }
+        )
+        console.log(this.props.data + ": " + this.state.qtdCliques);
+    }
+
+    render () {
+        return (
+            <button onClick={this.aoClicar}>
+                { 
+                    this.props.data > 0 && this.props.data < 32 ? 
+                    this.props.data : '-'}
+            </button>
+        );
+    }
+}
+
+/*
+function Dia({diaDaSemana, data}) {
     return (
-        <button>{diaDaSemana}</button>
+        <button>{ data > 0 && data < 32 ? data : '-'}</button>
     )
 }
+*/
 
 export default Dia;
