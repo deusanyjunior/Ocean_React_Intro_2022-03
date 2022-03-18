12b7f75ac11dcc549f2d2305829b139f714497fd
dj <dj@ime.usp.br>
2022-03-18 15:47:03 -0300

    Adicionando react-calendar

--- src/components/Corpo.js
+++ src/components/Corpo.js
@@ -1,9 +1,12 @@
 import Calendario from "./Calendario/Calendario";
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 
 function Corpo() {
     return (
         <div>
             <Calendario />
+            <Calendar calendarType="US" />
         </div>
     )
 }
